c4d42266d0319b433a291a750a4882be8b74fede
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-03-09 09:07:53 +0100

    Initial Commit

---
+++ RestoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RestoApp
+//
+//  Created by Ludovic Ollagnier on 09/03/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RestoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RestoApp
+//
+//  Created by Ludovic Ollagnier on 09/03/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
