ba8f615c7f50dfa92eaddbd7b2dfa5326a1e4d6e
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-03-10 10:17:04 +0100

    Added a custom UITableViewController subclass, and added it to the scene

---
+++ RestoApp/RestaurantTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RestaurantTableViewController.h
+//  RestoApp
+//
+//  Created by Ludovic Ollagnier on 10/03/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestaurantTableViewController : UITableViewController
+
+@end
