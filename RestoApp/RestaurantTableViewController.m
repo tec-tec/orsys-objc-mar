72a757bc2bb13caeafba61b4347d636aff0f9c78
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-03-10 10:24:30 +0100

    Added a RestaurantLibrary property with lazy instanciation

--- RestoApp/RestaurantTableViewController.m
+++ RestoApp/RestaurantTableViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "RestaurantTableViewController.h"
+#import "RestaurantLibrary.h"
 
 @interface RestaurantTableViewController ()
+
+@property (strong, nonatomic) RestaurantLibrary *library;
 
 @end
 
@@ -16,12 +19,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -52,40 +49,6 @@
 */
 
 /*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -94,5 +57,14 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (RestaurantLibrary *)library {
+
+    if (!_library) {
+        _library = [[RestaurantLibrary alloc] init];
+    }
+
+    return _library;
+}
 
 @end
